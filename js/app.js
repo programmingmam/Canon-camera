2388155df85e8942a6ab632ebc85d07df156d045
Mamun Rana <117065526+programmingmam@users.noreply.github.com>
2023-03-21 11:16:23 -0700

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,1 +1,43 @@
- 
+  
+
+
+//   clint review-slider
+
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
+  });
+
+   //scroll up button js//
+const scrollTopButton =document.querySelector('.scroll-top');
+
+scrollTopButton.addEventListener('click', ()=> {
+    window.scrollTo(0,0);
+})
+
+//scroll up button js//
